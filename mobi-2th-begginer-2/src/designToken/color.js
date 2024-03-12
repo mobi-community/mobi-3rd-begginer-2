c35219e09930e870a0f47f41263f3bc9ba7f7dd3
hayoung78 <wendy4332@naver.com>
2024-03-12 23:10:03 +0900

    feat: design-token 생성

---
+++ mobi-2th-begginer-2/src/designToken/color.js
@@ -0,0 +1,54 @@
+const SYSTEM = {
+    error: "#E50045",
+    alert: "#FFAA2A",
+    success: "#05B4FF",
+};
+
+//라벤더
+const MAIN = {
+    base: "#AC87C5", // 600 과 동일값
+    0: "#FFE5E5",
+    200: "#E0AED0",
+    400: "#B784B7",
+    600: "#AC87C5",
+    800: "#8E7AB5",
+    1000: "#756AB6",
+};
+const COMMON = {
+    0: "#000000",
+    200: "#363636",
+    400: "#6F6F6F",
+    600: "#A8A8A8",
+    800: "#D8D8D8",
+    900: "#F1F1F1",
+    1000: "#FFFFFF",
+};
+const PALETTE = {
+    teal: {
+        base: "#709FB0",
+        light: "#A0C1B8",
+    },
+    green: {
+        base: "#D7E4C0",
+        light: "#C6DCBA",
+        weight: "#BBC3A4",
+    },
+    gray: {
+        base: "#E3E1D9",
+        light: "#C7C8CC",
+        weight: "#B4B4B8",
+    },
+    peach: {
+        base: "#EFB495",
+        light: "#EFD595",
+        weight: "#154A65",
+    },
+    blue: "#7BD3EA",
+    lemon: "#F6F7C4",
+};
+export const COLOR = {
+    SYSTEM,
+    MAIN,
+    COMMON,
+    PALETTE,
+};
