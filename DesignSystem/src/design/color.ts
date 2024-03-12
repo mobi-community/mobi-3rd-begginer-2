7bb2018790a89bf96830471648965dc57f78be16
snkchan <144839872+snkchan@users.noreply.github.com>
2024-03-12 18:14:13 +0900

    style >> design-token등록 및 불피요한 파일 삭제

---
+++ DesignSystem/src/design/color.ts
@@ -0,0 +1,24 @@
+export const COLOR = {
+  grayScale: {
+    700: "#000000",
+    500: "292929",
+    400: "4e4e4e",
+    300: "777777",
+    200: "a2a2a2",
+    100: "d0d0d0",
+    0: "#ffffff",
+  },
+
+  theme: {
+    main: {
+      light: "#B5CFFA",
+      normal: "#6E99E5",
+      thick: "#2964C7",
+    },
+    mainOppsite: {
+      light: "#B1B773",
+      normal: "#A9B241",
+      thick: "#CDDB13",
+    },
+  },
+} as const
