494c219a1cf1516f2ef42d412fe2e0248a7b5237
snkchan <144839872+snkchan@users.noreply.github.com>
2024-03-13 19:34:30 +0900

    🎁 style >> 페이지이동을 위한 초기설정완료

---
+++ DesignSystem/src/page/components.tsx
@@ -0,0 +1,5 @@
+const Components = () => {
+  return <>gdgdg</>
+}
+
+export default Components
---
+++ DesignSystem/src/router/router.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom"
+import Main from "../page/main"
+import Components from "../page/components"
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <Main />,
+  },
+  {
+    path: "/components",
+    element: <Components />,
+  },
+])
+
+export default router
