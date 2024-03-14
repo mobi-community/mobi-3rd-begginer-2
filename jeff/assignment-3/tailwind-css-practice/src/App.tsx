e4e9177be6d37329bf032af0692a61f6190e102c
yunshin <2younsin@gmail.com>
2024-03-14 17:43:43 +0900

    🎁 style >> a3Button 공용 컴포넌트 생성

--- jeff/assignment-3/tailwind-css-practice/src/App.tsx
+++ jeff/assignment-3/tailwind-css-practice/src/App.tsx
@@ -1,9 +1,16 @@
+import { A3Button } from './components/A3Button'
+
 function App() {
 	return (
 		<>
-			<h1>Hello world!</h1>
+			<h1 className='text-primary-400 text-xl'>Hello world!</h1>
+			<A3Button variant='secondary' size='small' shape='full' weight='medium'>
+				관심공연
+			</A3Button>
+			<A3Button variant='primary' size='small' shape='full' weight='medium'>
+				홈페이지
+			</A3Button>
 		</>
 	)
 }
-
 export default App
