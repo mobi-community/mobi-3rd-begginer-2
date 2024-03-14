98f955c6256b568c148e73ee3ea90daa3a857813
snkchan <144839872+snkchan@users.noreply.github.com>
2024-03-15 02:23:30 +0900

    🪣 chore >> "class-variance-authority" cva설치

--- StyleLibrary/.eslintrc.cjs
+++ StyleLibrary/.eslintrc.cjs
@@ -12,6 +12,14 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
+    rules: {
+      "react/prop-types": [
+        "warn",
+        {
+          skipUndeclared: true,
+        },
+      ],
+    },
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
