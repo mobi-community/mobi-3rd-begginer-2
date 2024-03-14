140a8a0f804692cc0045ad927e0c73ae707d16bc
hayoung78 <wendy4332@naver.com>
2024-03-14 20:26:15 +0900

    feat:storybook 사용

--- mobi-2th-begginer-2/src/components/button.stories.js
+++ mobi-2th-begginer-2/src/components/button.stories.js
@@ -1,9 +1,19 @@
-import Button from "./button";
-
+import Button from "./common/button";
+//title: 컴포넌트이름
+//component: 컴포넌트
+//argTypes: 컴포넌트에 필요한 전달인자의 종류와 타입 , title이라는 전달인자에 text타입
 export default {
     title: "Button",
     component: Button,
-    argTypes:{
-        title: {control: "text"},
-    }
+    argTypes: {
+        text: { control: "text" },
+    },
+};
+//템플릿 생성. 템플릿을 사용하여 스토리북에 넣어줄 스토리
+const Template = (args) => <Button {...args} />;
+export const PurpleBtn = Template.bind({});
+PurpleBtn.args = {
+    size: "medium",
+    color: "mainPurple",
+    text: "나빼고 다 바보",
 };
--- mobi-2th-begginer-2/src/components/styleTheme/button.style.js
+++ mobi-2th-begginer-2/src/components/styleTheme/button.style.js
@@ -1,34 +1,78 @@
-import { css } from "styled-components";
+import styled, { css } from "styled-components";
 import { COLORS } from "../../designToken/color";
+import { SIZES } from "../../designToken/size";
 
-const ButtonColorCss= {
-    mainPuple: {css:css`
-    background-color: ${COLORS.MAIN.base};
-    color: ${COLORS.SYSTEM.black};
-     `},
-    teal: {css:css`
-    background-color: ${COLORS.PALLETE.teal.base};
-    color: ${COLORS.SYSTEM.black};
-    `},
-    green: {css:css`
-    background-color: ${COLORS.PALLETE.green.base};
-    color: ${COLORS.SYSTEM.black};
-   `},
-    gray: {css:css`
-    background-color: ${COLORS.PALLETE.gray.base};
-    color: ${COLORS.SYSTEM.black};
-    `},
-    peach: {css:css`
-    background-color: ${COLORS.PALLETE.peach};
-    color: ${COLORS.SYSTEM.black};
-    `},
-    lemon: {css:css`
-    background-color: ${COLORS.PALLETE.lemon};
-    color: ${COLORS.SYSTEM.black};
-    `},
-
-}
+const ButtonColorCss = {
+    mainPurple: {
+        css: css`
+            background-color: ${COLORS.MAIN.base};
+            color: ${COLORS.SYSTEM.black};
+        `,
+        activeColor: css`
+            background-color: ${COLORS.MAIN[200]};
+        `,
+    },
+    teal: {
+        css: css`
+            background-color: ${COLORS.PALLETE.teal.base};
+            color: ${COLORS.SYSTEM.black};
+        `,
+    },
+    green: {
+        css: css`
+            background-color: ${COLORS.PALLETE.green.base};
+            color: ${COLORS.SYSTEM.black};
+        `,
+    },
+    gray: {
+        css: css`
+            background-color: ${COLORS.PALLETE.gray.base};
+            color: ${COLORS.SYSTEM.black};
+        `,
+    },
+    peach: {
+        css: css`
+            background-color: ${COLORS.PALLETE.peach};
+            color: ${COLORS.SYSTEM.black};
+        `,
+    },
+    lemon: {
+        css: css`
+            background-color: ${COLORS.PALLETE.lemon};
+            color: ${COLORS.SYSTEM.black};
+        `,
+    },
+};
 
 const ButtonSizeCss = {
-    
-}
+    small: {
+        css: css`
+            width: ${SIZES.BUTTON.small.width};
+            height: ${SIZES.BUTTON.small.height};
+        `,
+    },
+    medium: {
+        css: css`
+            width: ${SIZES.BUTTON.medium.width};
+            height: ${SIZES.BUTTON.medium.height};
+        `,
+    },
+    large: {
+        css: css`
+            width: ${SIZES.BUTTON.large.width};
+            height: ${SIZES.BUTTON.large.height};
+        `,
+    },
+};
+
+export const StyleBtn = styled.button`
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    ${({ color }) => ButtonColorCss[color].css}
+    ${({ size }) => ButtonSizeCss[size].css}
+    &:active {
+        background-color: ${({ color }) => ButtonColorCss[color].activeColor};
+        transform: scale(0.8);
+    }
+`;
