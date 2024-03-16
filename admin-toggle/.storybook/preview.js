ccb67118b2ee8159800ce2bf10be9af6d2484ff5
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-03-17 01:10:16 +0900

    storybook/ react-router-dom 전역설정

--- admin-toggle/.storybook/preview.js
+++ admin-toggle/.storybook/preview.js
@@ -1,13 +1,22 @@
 /** @type { import('@storybook/react').Preview } */
+import { BrowserRouter } from "react-router-dom";
 const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
 };
 
 export default preview;
+
+export const decorators = [
+    (Story) => (
+        <BrowserRouter>
+            <Story />
+        </BrowserRouter>
+    ),
+];
