3469e4d93d64fc88481fd56ac96e4a59db645615
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-03-17 19:53:32 +0900

    주석 삭제 및 파일 생성

--- design-token/src/components/common/button.jsx
+++ design-token/src/components/common/button.jsx
@@ -1,14 +1,6 @@
 import { SButton } from "../style/button.style";
 
 const StyledButton = ({ theme, size, text, onClick }) => {
-    // const [buttonText, setButtonText] = useState(text);
-    // const [buttonTheme, setButtonTheme] = useState(theme);
-
-    // const handleClick = () => {
-    //     setButtonText((prev) => (prev === text ? clickText : text));
-    //     setButtonTheme((prev) => (prev === theme ? clickColor : theme));
-    // };
-
     return (
         <SButton theme={theme} size={size} onClick={onClick}>
             {text}
